505254a650bfed1638e13c4c639d2d01f4938740
Com-Sun <gnlslfflffl@gmail.com>
2022-12-31 17:29:28 +0900

    initialize repository

---
+++ src/main/java/com/nhnacademy/booklay/server/BooklayShopApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.booklay.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooklayShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooklayShopApplication.class, args);
+    }
+
+}

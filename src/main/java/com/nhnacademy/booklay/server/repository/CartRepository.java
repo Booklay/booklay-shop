e62eb44aa1975e295ce316492b8aea2ad8b69fbf
최규태 <kuitae0725@gmail.com>
2023-01-06 15:34:54 +0900

    feat : cart, post 분야의 repository 생성

---
+++ src/main/java/com/nhnacademy/booklay/server/repository/CartRepository.java
@@ -0,0 +1,8 @@
+package com.nhnacademy.booklay.server.repository;
+
+import com.nhnacademy.booklay.server.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository extends JpaRepository<Cart, Cart.Pk> {
+
+}

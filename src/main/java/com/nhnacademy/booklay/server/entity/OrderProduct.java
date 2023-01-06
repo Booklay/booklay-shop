142ec1c69b5f32fe8672709957ff516e02ac19bc
opgumo <opgumo@naver.com>
2023-01-06 16:20:19 +0900

    feat: couponRepositoryTest 추가.

--- src/main/java/com/nhnacademy/booklay/server/entity/OrderProduct.java
+++ src/main/java/com/nhnacademy/booklay/server/entity/OrderProduct.java
@@ -2,7 +2,6 @@ package com.nhnacademy.booklay.server.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -15,7 +14,6 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Table(name = "order_product")
 @Entity
--- src/test/java/com/nhnacademy/booklay/server/repository/CouponRepositoryTest.java
+++ src/test/java/com/nhnacademy/booklay/server/repository/CouponRepositoryTest.java
@@ -1,10 +1,16 @@
 package com.nhnacademy.booklay.server.repository;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import com.nhnacademy.booklay.server.dummy.Dummy;
+import com.nhnacademy.booklay.server.entity.Coupon;
 import javax.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
@@ -13,14 +19,41 @@ import org.springframework.test.context.ActiveProfiles;
 class CouponRepositoryTest {
 
     @Autowired
+    TestEntityManager entityManager;
+
+    @Autowired
     CouponRepository couponRepository;
 
-    @BeforeEach
-    void setUp() {
+    @Test
+    @DisplayName("couponRepository save test")
+    void testCouponSave() {
+
+        //given
+        Coupon coupon = Dummy.getDummyCoupon();
+        entityManager.persist(coupon.getImage());
+        entityManager.persist(coupon.getCouponType());
+
+        //when
+        Coupon expected = couponRepository.save(coupon);
+
+        //then
+        assertThat(expected.getName()).isEqualTo(coupon.getName());
     }
 
     @Test
-    void testCouponSave() {
-    }
+    @DisplayName("couponRepository save test")
+    void testCouponFindById() {
 
+        //given
+        Coupon coupon = Dummy.getDummyCoupon();
+        entityManager.persist(coupon.getImage());
+        entityManager.persist(coupon.getCouponType());
+        couponRepository.save(coupon);
+
+        //when
+        Coupon expected = couponRepository.findById(coupon.getId()).orElseThrow(() -> new IllegalArgumentException("Coupon not found."));
+
+        //then
+        assertThat(expected.getId()).isEqualTo(coupon.getId());
+    }
 }

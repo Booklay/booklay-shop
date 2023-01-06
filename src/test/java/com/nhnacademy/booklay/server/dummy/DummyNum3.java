beabce91ac96456b3a47cf733aabcd88257df5fb
DESKTOP-1MVJTC3\ohjoonhoo <ojh789ojh@naver.com>
2023-01-06 17:39:14 +0900

    test 2종 추가

--- src/test/java/com/nhnacademy/booklay/server/dummy/DummyNum3.java
+++ src/test/java/com/nhnacademy/booklay/server/dummy/DummyNum3.java
@@ -1,30 +1,70 @@
 package com.nhnacademy.booklay.server.dummy;
 
+import com.nhnacademy.booklay.server.entity.Image;
 import com.nhnacademy.booklay.server.entity.OwnedEbook;
+import com.nhnacademy.booklay.server.entity.Product;
 import com.nhnacademy.booklay.server.entity.RestockingNotification;
+import java.time.LocalDateTime;
 import org.springframework.test.util.ReflectionTestUtils;
 
 public class DummyNum3 {
+
+
+    private static Product getDummyProductForDummy(){
+        Image image = Image.builder()
+            .address("")
+            .ext("")
+            .build();
+        ReflectionTestUtils.setField(image, "id", 1L);
+        Product product = Product.builder()
+            .image(image)
+            .price(1)
+            .pointRate(1)
+            .isSelling(true)
+            .longDescription("")
+            .pointMethod(true)
+            .registedAt(LocalDateTime.now())
+            .title("")
+            .shortDescription("")
+            .build();
+        ReflectionTestUtils.setField(product, "id", 1L);
+        return product;
+    }
     public static RestockingNotification getDummyRestockingNotification() {
-
-
+        Image image = Image.builder()
+            .address("")
+            .ext("")
+            .build();
+        ReflectionTestUtils.setField(image, "id", 1L);
+        Product product = Product.builder()
+            .image(image)
+            .price(1)
+            .pointRate(1)
+            .isSelling(true)
+            .longDescription("")
+            .pointMethod(true)
+            .registedAt(LocalDateTime.now())
+            .title("")
+            .shortDescription("")
+            .build();
+        ReflectionTestUtils.setField(product, "id", 1L);
         RestockingNotification restockingNotification = RestockingNotification.builder()
-            .product(null)
-            .member(null)
+            .product(product)
+            .member(Dummy.getDummyMember())
             .build();
 
-        ReflectionTestUtils.setField(restockingNotification, "restockingNotificationId", new RestockingNotification.Pk(null, null));
+        ReflectionTestUtils.setField(restockingNotification, "pk", new RestockingNotification.Pk(null, null));
 
         return restockingNotification;
     }
 
     public static OwnedEbook getDummyOwnedEbook() {
         OwnedEbook ownedEbook = OwnedEbook.builder()
-            .product(null)
-            .member(null)
+            .product(getDummyProductForDummy())
+            .member(Dummy.getDummyMember())
             .build();
 
-        ReflectionTestUtils.setField(ownedEbook, "ownedEbookId", 1L);
+        ReflectionTestUtils.setField(ownedEbook, "id", 1L);
 
         return ownedEbook;
     }

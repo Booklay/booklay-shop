da442e3565b7e9072d88b60385f86803542f1f35
SEONG JUN LEE <devel0624@gmail.com>
2023-01-06 13:57:31 +0900

    feat: entity 추가 DeliveryDetail, DeliveryStatusCode, ProductRelation, ProductTag, Review, Subscribe

---
+++ src/main/java/com/nhnacademy/booklay/server/entity/ProductTag.java
@@ -0,0 +1,59 @@
+package com.nhnacademy.booklay.server.entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "product_tag")
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ProductTag {
+
+    @EmbeddedId
+    private Pk pk;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_no")
+    @MapsId("productId")
+    private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tag_no")
+    @MapsId("tagId")
+    private Tag tag;
+
+    @Embeddable
+    @EqualsAndHashCode
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Pk implements Serializable {
+
+        @Column(name = "product_no", nullable = false)
+        private Integer productId;
+
+        @Column(name = "tag_no", nullable = false)
+        private Integer tagId;
+
+    }
+
+    @Builder
+    public ProductTag(Pk pk, Product product, Tag tag) {
+        this.pk = pk;
+        this.product = product;
+        this.tag = tag;
+    }
+}
---
+++ src/main/java/com/nhnacademy/booklay/server/entity/Subscribe.java
@@ -0,0 +1,47 @@
+package com.nhnacademy.booklay.server.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "subscribe")
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Subscribe {
+
+    @Id
+    @Column(name = "product_no")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_no", nullable = false)
+    private Product product;
+
+    @Column(name = "subscribe_week", nullable = false)
+    private int subscribeWeek;
+
+    @Column(name = "subscribe_day", nullable = false)
+    private int subscribeDay;
+
+    @Setter
+    @Column(name = "publisher", length = 100)
+    private String publisher;
+
+    @Builder
+    public Subscribe(Long id, Product product, int subscribeWeek, int subscribeDay) {
+        this.id = id;
+        this.product = product;
+        this.subscribeWeek = subscribeWeek;
+        this.subscribeDay = subscribeDay;
+    }
+}

c5aa5c72c068dfddb958dd0bb4a697dd65d96514
SEONG JUN LEE <devel0624@gmail.com>
2023-01-08 19:05:48 +0900

    feat: Category AdminController 추가 및 기타 수정 repository 페이지네이션 대응 추가 service 인터페이스 추가 및 기존 서비스는 Impl로 변경 후 service 인터페이스를 구현 컨트롤러는 등록, 수정, 검색, 삭제, 페이지네이션 추가

--- src/main/java/com/nhnacademy/booklay/server/repository/CategoryRepository.java
+++ src/main/java/com/nhnacademy/booklay/server/repository/CategoryRepository.java
@@ -3,6 +3,8 @@ package com.nhnacademy.booklay.server.repository;
 
 import com.nhnacademy.booklay.server.entity.Category;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
@@ -11,4 +13,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     <T> Optional<T> findById(Long id, Class<T> type);
+
+    <T> Page<T> findAllBy(Pageable pageable, Class<T> type);
+
 }

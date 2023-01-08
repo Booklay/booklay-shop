b81134c0a6e4f8d706e5699ad895638b2c7c7c97
SEONG JUN LEE <devel0624@gmail.com>
2023-01-08 19:05:48 +0900

    feat: Category AdminController 추가 및 기타 수정 repository 페이지네이션 대응 추가 service 인터페이스 추가 및 기존 서비스는 Impl로 변경 후 service 인터페이스를 구현 컨트롤러는 등록, 수정, 검색, 삭제, 페이지네이션 추가

--- src/main/java/com/nhnacademy/booklay/server/service/category/CategoryService.java
+++ src/main/java/com/nhnacademy/booklay/server/service/category/CategoryService.java
@@ -1,99 +1,19 @@
 package com.nhnacademy.booklay.server.service.category;
 
 import com.nhnacademy.booklay.server.dto.category.CategoryCreateDto;
-import com.nhnacademy.booklay.server.dto.category.CategoryCUDto;
 import com.nhnacademy.booklay.server.dto.category.CategoryDto;
 import com.nhnacademy.booklay.server.dto.category.CategoryUpdateDto;
-import com.nhnacademy.booklay.server.entity.Category;
-import com.nhnacademy.booklay.server.exception.category.CreateCategoryFailedException;
-import com.nhnacademy.booklay.server.exception.category.CategoryNotFoundException;
-import com.nhnacademy.booklay.server.exception.category.UpdateCategoryFailedException;
-import com.nhnacademy.booklay.server.repository.CategoryRepository;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-/**
- * javadoc. 카테고리 서비스
- */
-@Slf4j
-@Transactional(readOnly = true)
-@Service
-public class CategoryService {
+public interface CategoryService {
+    void createCategory(CategoryCreateDto createDto);
 
-    private final CategoryRepository categoryRepository;
+    CategoryDto retrieveCategory(Long id);
 
-    public CategoryService(CategoryRepository categoryRepository) {
-        this.categoryRepository = categoryRepository;
-    }
+    void updateCategory(CategoryUpdateDto updateDto);
 
-    @Transactional(readOnly = false)
-    public void createCategory(CategoryCreateDto createDto) {
-        if (!categoryRepository.existsById(createDto.getId())) {
-            log.info("Create Category Processing");
-            if (!taskForCategoryFromDto(createDto)) {
-                log.info("Create Category Failed");
-                throw new CreateCategoryFailedException();
-            }
-        } else {
-            log.info("Category ID Already Existed");
-        }
-    }
+    boolean deleteCategory(Long id);
 
-    public CategoryDto retrieveCategory(Long categoryId) {
-        return categoryRepository.findById(categoryId, CategoryDto.class)
-            .orElseThrow(CategoryNotFoundException::new);
-    }
-
-    @Transactional(readOnly = false)
-    public void updateCategory(CategoryUpdateDto updateDto) {
-        if (categoryRepository.existsById(updateDto.getId())) {
-            log.info("Update Category Processing");
-            if (!taskForCategoryFromDto(updateDto)) {
-                log.info("Update Category Failed");
-                throw new UpdateCategoryFailedException();
-            }
-        } else {
-            log.info("Category ID Not Existed");
-            throw new CategoryNotFoundException();
-        }
-    }
-
-    @Transactional(readOnly = false)
-    public void deleteCategory(Long categoryId) {
-        if (categoryRepository.existsById(categoryId)) {
-            categoryRepository.deleteById(categoryId);
-        } else {
-            log.info("Category ID Not Existed");
-            throw new CategoryNotFoundException();
-        }
-    }
-
-    private boolean taskForCategoryFromDto(CategoryCUDto dto) {
-
-        try {
-            Category parent = categoryRepository.findById(dto.getParentCategoryId())
-                .orElseThrow(CategoryNotFoundException::new);
-
-            Category category = Category.builder()
-                .id(dto.getId())
-                .parent(parent)
-                .name(dto.getName())
-                .depth(parent.getDepth() + 1L)
-                .isExposure(dto.getIsExposure())
-                .build();
-
-            categoryRepository.save(category);
-
-            return true;
-        } catch (Exception e) {
-            log.info(
-                "Exception Occurred : " + e.getMessage()
-                    + "\nException Caused By " + e.getCause()
-            );
-            return false;
-        }
-
-    }
-
+    Page<CategoryDto> retrieveCategory(Pageable pageable);
 }

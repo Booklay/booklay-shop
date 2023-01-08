ee5af525756aaee7d1eec512df283b46c7387651
opgumo <opgumo@naver.com>
2023-01-08 23:57:46 +0900

    feat: 기본 crud 기능 정의.

---
+++ src/main/java/com/nhnacademy/booklay/server/controller/admin/CouponAdminController.java
@@ -0,0 +1,41 @@
+package com.nhnacademy.booklay.server.controller.admin;
+
+import com.nhnacademy.booklay.server.service.coupon.CouponAdminService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class CouponAdminController {
+
+    private final CouponAdminService couponAdminService;
+
+    @GetMapping("/coupons")
+    public String retrieveAllCoupons() {
+        couponAdminService.retrieveAllCoupons();
+        return "";
+    }
+
+    @GetMapping("/coupons/{couponId}")
+    public String retrieveCoupon(@PathVariable Long couponId) {
+        couponAdminService.retrieveCoupon(couponId);
+        return "";
+    }
+
+    @PutMapping("/coupons/{couponId}")
+    public String updateCoupon(@PathVariable Long couponId) {
+        //TODO: @RequestBody
+        couponAdminService.updateCoupon(couponId);
+        return "";
+    }
+
+    @DeleteMapping("/coupons/{couponId}")
+    public void deleteCoupon(@PathVariable Long couponId) {
+        couponAdminService.deleteCoupon(couponId);
+    }
+}
---
+++ src/main/java/com/nhnacademy/booklay/server/service/coupon/CouponAdminService.java
@@ -0,0 +1,37 @@
+package com.nhnacademy.booklay.server.service.coupon;
+
+import com.nhnacademy.booklay.server.entity.Coupon;
+import com.nhnacademy.booklay.server.repository.CouponRepository;
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CouponAdminService {
+
+    private final CouponRepository couponRepository;
+
+    @Transactional(readOnly = true)
+    public void retrieveAllCoupons() {
+        //TODO : couponResponse object
+        List<Coupon> couponList = couponRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public void retrieveCoupon(Long couponId) {
+        //TODO : couponDetailResponse object
+        Coupon coupon = couponRepository.findById(couponId).orElseThrow(() -> new IllegalArgumentException("No Such Coupon."));
+    }
+
+    @Transactional
+    public void updateCoupon(Long couponId) {
+    }
+
+    @Transactional
+    public void deleteCoupon(Long couponId) {
+        couponRepository.deleteById(couponId);
+    }
+}

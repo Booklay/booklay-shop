1026fbeddae6e57bdf7383d9ce965c82926e3615
yangrystal <tmddk512@naver.com>
2023-01-09 10:56:26 +0900

    feat:member read 재수정

--- src/main/java/com/nhnacademy/booklay/server/controller/member/MemberController.java
+++ src/main/java/com/nhnacademy/booklay/server/controller/member/MemberController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +23,7 @@ public class MemberController {
     }
 
     @GetMapping("/{memberId}")
-    private MemberDto getMember(@PathVariable long memberId){
+    private MemberDto getMember(@PathVariable long memberId) {
         return memberService.getMember(memberId);
     }
 }

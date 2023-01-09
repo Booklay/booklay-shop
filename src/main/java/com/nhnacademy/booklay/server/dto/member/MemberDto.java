09d0d788da7ec3c4c17f14999868ace1f349fc65
yangrystal <tmddk512@naver.com>
2023-01-09 11:14:37 +0900

    fix:member read annotation, method naming 수정

--- src/main/java/com/nhnacademy/booklay/server/dto/member/MemberDto.java
+++ src/main/java/com/nhnacademy/booklay/server/dto/member/MemberDto.java
@@ -3,7 +3,12 @@ package com.nhnacademy.booklay.server.dto.member;
 import com.nhnacademy.booklay.server.entity.Gender;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberDto {
     private Long memberId;
     private Gender gender;

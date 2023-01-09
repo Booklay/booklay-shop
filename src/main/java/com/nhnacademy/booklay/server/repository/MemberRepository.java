1cf4cc833372bdc0b673e71ab41d248cbd2a8e48
yangrystal <tmddk512@naver.com>
2023-01-09 11:14:37 +0900

    fix:member read annotation, method naming 수정

--- src/main/java/com/nhnacademy/booklay/server/repository/MemberRepository.java
+++ src/main/java/com/nhnacademy/booklay/server/repository/MemberRepository.java
@@ -4,9 +4,7 @@ import com.nhnacademy.booklay.server.dto.member.MemberDto;
 import com.nhnacademy.booklay.server.entity.Member;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
     List<MemberDto> findAllBy();
     MemberDto findByMemberId(Long memberId);

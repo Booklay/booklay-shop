a7170e3a2aa83c607d829dce4b1d44b9c0bf4330
opgumo <opgumo@naver.com>
2023-01-10 01:37:03 +0900

    remove useless package

--- src/test/java/com/nhnacademy/booklay/server/repository/CouponRepositoryTest.java
+++ src/test/java/com/nhnacademy/booklay/server/repository/CouponRepositoryTest.java
@@ -17,7 +17,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.util.ReflectionTestUtils;
 
 @DataJpaTest
 @ActiveProfiles("test")

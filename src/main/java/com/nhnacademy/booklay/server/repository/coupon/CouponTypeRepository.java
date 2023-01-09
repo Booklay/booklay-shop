b0e18da098cfd184542502c53c5abcedc1ddeda9
opgumo <opgumo@naver.com>
2023-01-10 00:19:05 +0900

    fix: CouponCURequest수정, CouponService Create 추가

---
+++ src/main/java/com/nhnacademy/booklay/server/repository/coupon/CouponTypeRepository.java
@@ -0,0 +1,7 @@
+package com.nhnacademy.booklay.server.repository.coupon;
+
+import com.nhnacademy.booklay.server.entity.CouponType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CouponTypeRepository extends JpaRepository<CouponType, Long> {
+}

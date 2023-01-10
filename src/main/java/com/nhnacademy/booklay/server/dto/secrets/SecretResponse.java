3615a67c95f3100e4b9a4d029197d62bc8e4fc1c
Com-Sun <gnlslfflffl@gmail.com>
2023-01-10 16:37:50 +0900

    fix: 패스워드 분리

--- src/main/java/com/nhnacademy/booklay/server/dto/secrets/SecretResponse.java
+++ src/main/java/com/nhnacademy/booklay/server/dto/secrets/SecretResponse.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Getter
 @NoArgsConstructor

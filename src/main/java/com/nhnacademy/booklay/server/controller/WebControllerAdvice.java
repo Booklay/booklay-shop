317cc536c36af8b77b23b5e3ecafe3b992fe2c45
devel0624 <devel0624@gmail.com>
2023-01-11 08:51:01 +0900

    refactor: exception refactoring 반영

--- src/main/java/com/nhnacademy/booklay/server/controller/WebControllerAdvice.java
+++ src/main/java/com/nhnacademy/booklay/server/controller/WebControllerAdvice.java
@@ -1,45 +1,103 @@
 package com.nhnacademy.booklay.server.controller;
 
-import com.nhnacademy.booklay.server.exception.controller.CreateFailedException;
-import com.nhnacademy.booklay.server.exception.controller.DeleteFailedException;
-import com.nhnacademy.booklay.server.exception.controller.UpdateFailedException;
-import com.nhnacademy.booklay.server.exception.service.NotFoundException;
+import com.nhnacademy.booklay.server.dto.ErrorResponse;
+import com.nhnacademy.booklay.server.exception.ApiException;
+import com.nhnacademy.booklay.server.exception.CommonErrorCode;
+import com.nhnacademy.booklay.server.exception.ErrorCode;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 애플리케이션에서 발생하는 전반적인 Error 처리를 위한 컨트롤러입니다.
+ *
+ * @author 조현진
+ */
 @Slf4j
-@ControllerAdvice
-public class WebControllerAdvice {
+@RestControllerAdvice
+public class WebControllerAdvice extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(value = {
-        CreateFailedException.class,
-        UpdateFailedException.class,
-        DeleteFailedException.class,
-        NotFoundException.class
-    })
-    @ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "잘못된 요청 정보")
-    public String createFailedException(Exception ex) {
-        return treatException(ex);
+    @ExceptionHandler(ApiException.class)
+    public ResponseEntity<Object> handleCustomException(ApiException e) {
+        ErrorCode errorCode = e.getErrorCode();
+        return handleExceptionInternal(errorCode);
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "필수 입력 값이 없거나 잘못된 값이 존재합니다.")
-    public String invalidRequestException(Exception ex) {
-        return treatException(ex);
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Object> handleIllegalArgument(IllegalArgumentException e) {
+        log.warn("handleIllegalArgument", e);
+        ErrorCode errorCode = CommonErrorCode.INVALID_PARAMETER;
+        return handleExceptionInternal(errorCode, e.getMessage());
     }
 
-    @ExceptionHandler(Exception.class)
-    public String handleException(Exception ex) {
-        return treatException(ex);
+    @Override
+    public ResponseEntity<Object> handleMethodArgumentNotValid(
+        MethodArgumentNotValidException e,
+        HttpHeaders headers,
+        HttpStatus status,
+        WebRequest request) {
+        log.warn("handleIllegalArgument", e);
+        ErrorCode errorCode = CommonErrorCode.INVALID_PARAMETER;
+        return handleExceptionInternal(e, errorCode);
     }
 
-    private String treatException(Exception ex) {
-        log.error("Exception Class : " + ex.getClass());
-        log.error("Exception Message : " + ex.getMessage());
-        return ex.getMessage();
+    @ExceptionHandler({Exception.class})
+    public ResponseEntity<Object> handleAllException(Exception ex) {
+        log.warn("handleAllException", ex);
+        ErrorCode errorCode = CommonErrorCode.INTERNAL_SERVER_ERROR;
+        return handleExceptionInternal(errorCode);
+    }
+
+    private ResponseEntity<Object> handleExceptionInternal(ErrorCode errorCode) {
+        return ResponseEntity.status(errorCode.getHttpStatus())
+            .body(makeErrorResponse(errorCode));
+    }
+
+    private ErrorResponse makeErrorResponse(ErrorCode errorCode) {
+        return ErrorResponse.builder()
+            .code(errorCode.name())
+            .message(errorCode.getMessage())
+            .build();
+    }
+
+    private ResponseEntity<Object> handleExceptionInternal(ErrorCode errorCode, String message) {
+        return ResponseEntity.status(errorCode.getHttpStatus())
+            .body(makeErrorResponse(errorCode, message));
+    }
+
+    private ErrorResponse makeErrorResponse(ErrorCode errorCode, String message) {
+        return ErrorResponse.builder()
+            .code(errorCode.name())
+            .message(message)
+            .build();
+    }
+
+    private ResponseEntity<Object> handleExceptionInternal(BindException e, ErrorCode errorCode) {
+        return ResponseEntity.status(errorCode.getHttpStatus())
+            .body(makeErrorResponse(e, errorCode));
+    }
+
+    private ErrorResponse makeErrorResponse(BindException e, ErrorCode errorCode) {
+        List<ErrorResponse.ValidationError> validationErrorList = e.getBindingResult()
+            .getFieldErrors()
+            .stream()
+            .map(ErrorResponse.ValidationError::of)
+            .collect(Collectors.toList());
+
+        return ErrorResponse.builder()
+            .code(errorCode.name())
+            .message(errorCode.getMessage())
+            .errors(validationErrorList)
+            .build();
     }
 }

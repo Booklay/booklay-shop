624ff734641defea6fd39236cd6836ef7bc1e622
최규태 <kuitae0725@gmail.com>
2023-01-10 13:46:46 +0900

    entity : listedAt 자동화

--- src/main/java/com/nhnacademy/booklay/server/entity/Wishlist.java
+++ src/main/java/com/nhnacademy/booklay/server/entity/Wishlist.java
@@ -1,27 +1,32 @@
 package com.nhnacademy.booklay.server.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Table
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class Wishlist {
 
     @EmbeddedId
     private Pk pk;
 
+    @CreatedDate
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @Column(name = "listed_at")
     private LocalDateTime listedAt;
 
     @Builder
-    public Wishlist(Pk pk, LocalDateTime listedAt) {
+    public Wishlist(Pk pk) {
         this.pk = pk;
-        this.listedAt = listedAt;
     }
 
     @Embeddable

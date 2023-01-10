06f70aa0658fd57f7f27febd0cbecedef035560a
Com-Sun <gnlslfflffl@gmail.com>
2023-01-10 10:51:52 +0900

    feat: datasource 설정 및 securemanager 적용

--- src/main/java/com/nhnacademy/booklay/server/config/DatabaseConfig.java
+++ src/main/java/com/nhnacademy/booklay/server/config/DatabaseConfig.java
@@ -1,6 +1,8 @@
 package com.nhnacademy.booklay.server.config;
 
 import com.nhnacademy.booklay.server.dto.secrets.SecretResponse;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -21,22 +23,29 @@ public class DatabaseConfig {
     @Value("${booklay.secure.db_username}")
     private String username;
 
+    @Value("${booklay.secure.db_password}")
+    private String password;
+
+    @Value("${booklay.secure.db_url}")
+    private String dbUrl;
+
     @Bean
     public DataSource dataSource(RestTemplate restTemplate) {
 
         SecretResponse usernameResponse = restTemplate.getForObject(url + this.username, SecretResponse.class);
-        String username = usernameResponse.getBody().getSecret();
+        SecretResponse passwordResponse = restTemplate.getForObject(url + this.password, SecretResponse.class);
+        SecretResponse dbUrlResponse = restTemplate.getForObject(url + this.dbUrl, SecretResponse.class);
 
-        log.info("=============");
-        log.info(username);
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        hikariConfig.setJdbcUrl(dbUrlResponse.getBody().getSecret());
+        hikariConfig.setUsername(usernameResponse.getBody().getSecret());
+        hikariConfig.setPassword(passwordResponse.getBody().getSecret());
 
-        DataSource datasource = DataSourceBuilder.create()
-                .url("jdbc:mysql://133.186.151.141:3306/booklay_shop?serverTimezone=Asia/Seoul")
-                .password("_nys_6*0osSnt5Pa")
-                .driverClassName("com.mysql.cj.jdbc.Driver")
-                .username("booklay")
-                .build();
-        return datasource;
+        HikariDataSource hikariDataSource = new HikariDataSource(hikariConfig);
+        hikariDataSource.setMaximumPoolSize(2);
+
+        return hikariDataSource;
     }
 
 }

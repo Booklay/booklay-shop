9c2ce4a4a319bae6552a5325485fdac42bc55734
yangrystal <tmddk512@naver.com>
2023-01-11 18:37:46 +0900

    fix: MemberAdminController TC 수정

--- src/test/java/com/nhnacademy/booklay/server/controller/member/MemberAdminControllerTest.java
+++ src/test/java/com/nhnacademy/booklay/server/controller/member/MemberAdminControllerTest.java
@@ -1,5 +1,6 @@
 package com.nhnacademy.booklay.server.controller.member;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -10,12 +11,14 @@ import com.nhnacademy.booklay.server.dummy.Dummy;
 import com.nhnacademy.booklay.server.entity.Member;
 import com.nhnacademy.booklay.server.exception.member.MemberNotFoundException;
 import com.nhnacademy.booklay.server.service.member.MemberService;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -64,6 +67,10 @@ class MemberAdminControllerTest {
     @Test
     @DisplayName("Controller 연결 테스트")
     void testMapping() throws Exception {
+        //given
+        PageImpl<MemberRetrieveResponse> page = new PageImpl<>(List.of());
+        when(memberService.retrieveMembers(any())).thenReturn(page);
+
         //then
         mockMvc.perform(get(URI_PREFIX))
             .andExpect(status().isOk())

6cb56415ee89e226675cb992653a1c3227106377
opgumo <opgumo@naver.com>
2023-01-12 09:04:05 +0900

    fix: remove unused import package

--- src/test/java/com/nhnacademy/booklay/server/service/coupon/CouponAdminServiceImplTest.java
+++ src/test/java/com/nhnacademy/booklay/server/service/coupon/CouponAdminServiceImplTest.java
@@ -22,7 +22,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;

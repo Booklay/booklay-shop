6cb0e75e69018af23f26587c80a6a0ea22c1645f
최규태 <kuitae0725@gmail.com>
2023-01-13 09:57:43 +0900

    feat : 작가 프론트와 연동 시도

--- src/main/java/com/nhnacademy/booklay/server/repository/product/AuthorRepository.java
+++ src/main/java/com/nhnacademy/booklay/server/repository/product/AuthorRepository.java
@@ -8,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 
-  Page<RetrieveAuthorResponse> findAllBy(Pageable pageable, Class<RetrieveAuthorResponse> retrieveAuthorResponseClass);
+  <T> Page<T> findAllBy(Pageable pageable, Class<T> type);
 }

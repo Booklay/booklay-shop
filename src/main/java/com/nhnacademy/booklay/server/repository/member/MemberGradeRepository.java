d62e0c243ead16bbc82a81fe01402b41fce9e6f6
yangrystal <tmddk512@naver.com>
2023-01-13 16:54:46 +0900

    feat: Member Controller, Service에 Member Authority 삭제, Member Grade 생성, Member Grade Retrieve 구현

--- src/main/java/com/nhnacademy/booklay/server/repository/member/MemberGradeRepository.java
+++ src/main/java/com/nhnacademy/booklay/server/repository/member/MemberGradeRepository.java
@@ -1,7 +1,11 @@
 package com.nhnacademy.booklay.server.repository.member;
 
+import com.nhnacademy.booklay.server.dto.member.reponse.MemberGradeRetrieveResponse;
 import com.nhnacademy.booklay.server.entity.MemberGrade;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberGradeRepository extends JpaRepository<MemberGrade, Long> {
+    Page<MemberGradeRetrieveResponse> findByMember_MemberNo(Pageable pageable, Long memberNo);
 }

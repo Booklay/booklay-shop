b9c8869d7f5f0e3fa8a4629e53e0677fcb366ee2
opgumo <opgumo@naver.com>
2023-01-13 16:48:27 +0900

    feat: 주문 쿠폰 발급 서비스 테스트

--- src/main/java/com/nhnacademy/booklay/server/dto/coupon/CouponIssueRequest.java
+++ src/main/java/com/nhnacademy/booklay/server/dto/coupon/CouponIssueRequest.java
@@ -1,11 +1,13 @@
 package com.nhnacademy.booklay.server.dto.coupon;
 
 import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class CouponIssueRequest {
 
     @NotNull

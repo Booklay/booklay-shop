c95e388d09669e3bae6990d2b013387e258980c1
DESKTOP-1MVJTC3\ohjoonhoo <ojh789ojh@naver.com>
2023-01-15 20:08:49 +0900

    CouponBirthday batch 임시 transactionManager 관련문제 해결필요 충돌가능성 낮음

--- src/main/java/com/nhnacademy/booklay/server/BooklayShopApplication.java
+++ src/main/java/com/nhnacademy/booklay/server/BooklayShopApplication.java
@@ -3,7 +3,10 @@ package com.nhnacademy.booklay.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+@EnableBatchProcessing
+@EnableScheduling
 @SpringBootApplication
 @EnableJpaAuditing
 public class BooklayShopApplication {

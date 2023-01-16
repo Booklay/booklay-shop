95a47ec4b6d0c751d8f6891a51f45609a32ed1a4
Com-Sun <gnlslfflffl@gmail.com>
2023-01-16 20:32:25 +0900

    feat: 헬스체크용 컨트롤러 구현

---
+++ src/main/java/com/nhnacademy/booklay/server/controller/HealthController.java
@@ -0,0 +1,29 @@
+package com.nhnacademy.booklay.server.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+    private boolean healthStatus = true;
+
+    @GetMapping
+    public ResponseEntity<String> healthCheck() {
+        if (healthStatus) {
+            return ResponseEntity.ok("OK");
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("BAD_REQUEST");
+    }
+
+    @GetMapping("/deploy/ready")
+    public void makeHealthCheckFail() {
+        this.healthStatus = false;
+    }
+
+}

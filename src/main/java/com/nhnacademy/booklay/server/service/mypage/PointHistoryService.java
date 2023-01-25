2be1bc716015708728a5d645aae84bddeae021ab
yangrystal <tmddk512@naver.com>
2023-01-25 09:39:02 +0900

    feat: 가장 최근 Total Point 조회하는 기능 구현

--- src/main/java/com/nhnacademy/booklay/server/service/mypage/PointHistoryService.java
+++ src/main/java/com/nhnacademy/booklay/server/service/mypage/PointHistoryService.java
@@ -1,6 +1,7 @@
 package com.nhnacademy.booklay.server.service.mypage;
 
 import com.nhnacademy.booklay.server.dto.member.reponse.PointHistoryRetrieveResponse;
+import com.nhnacademy.booklay.server.dto.member.reponse.TotalPointRetrieveResponse;
 import com.nhnacademy.booklay.server.dto.member.request.PointHistoryCreateRequest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,4 +10,6 @@ public interface PointHistoryService {
     Page<PointHistoryRetrieveResponse> retrievePointHistorys(Pageable pageable);
 
     void createPointHistory(PointHistoryCreateRequest pointHistoryCreateRequest);
+
+    TotalPointRetrieveResponse retrieveTotalPoint(Long memberNo);
 }

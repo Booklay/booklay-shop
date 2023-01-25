12afecc573bbbbb9cfd43af7cb74cc0b1dd23360
최규태 <kuitae0725@gmail.com>
2023-01-25 21:59:29 +0900

    feat : 상품 수정 개발 queryDsl #1

--- src/main/java/com/nhnacademy/booklay/server/repository/product/ProductDetailRepositoryImpl.java
+++ src/main/java/com/nhnacademy/booklay/server/repository/product/ProductDetailRepositoryImpl.java
@@ -2,9 +2,13 @@ package com.nhnacademy.booklay.server.repository.product;
 
 import com.nhnacademy.booklay.server.dto.member.reponse.MemberForAuthorResponse;
 import com.nhnacademy.booklay.server.dto.product.author.response.RetrieveAuthorResponse;
+import com.nhnacademy.booklay.server.dto.product.response.RetrieveProductBookResponse;
 import com.nhnacademy.booklay.server.entity.ProductAuthor;
 import com.nhnacademy.booklay.server.entity.QAuthor;
+import com.nhnacademy.booklay.server.entity.QCategory;
+import com.nhnacademy.booklay.server.entity.QCategoryProduct;
 import com.nhnacademy.booklay.server.entity.QMember;
+import com.nhnacademy.booklay.server.entity.QProduct;
 import com.nhnacademy.booklay.server.entity.QProductAuthor;
 import com.nhnacademy.booklay.server.entity.QProductDetail;
 import com.querydsl.core.types.Projections;
@@ -31,5 +35,45 @@ public class ProductDetailRepositoryImpl extends QuerydslRepositorySupport imple
         .select(Projections.constructor(RetrieveAuthorResponse.class, author.authorId, author.name
             ,(Projections.constructor(MemberForAuthorResponse.class ,member.memberNo, member.memberId))
         )).fetch();
+  }
+
+  @Override
+  public RetrieveProductBookResponse findProductBookDateByDetailId(Long id) {
+    QProductDetail productDetail = QProductDetail.productDetail;
+    QProduct product = QProduct.product;
+    QProductAuthor productAuthor = QProductAuthor.productAuthor;
+    QAuthor author = QAuthor.author;
+    QCategoryProduct categoryProduct = QCategoryProduct.categoryProduct;
+    QCategory category = QCategory.category;
+
+    return from(productDetail)
+        .innerJoin(product).on(productDetail.product.id.eq(product.id))
+        .where(productDetail.id.eq(id))
+        .select(Projections.constructor(RetrieveProductBookResponse.class,
+            product.id,
+            product.title,
+            product.price,
+            product.pointRate,
+            product.shortDescription,
+            product.longDescription,
+            product.isSelling,
+            product.pointMethod,
+            productDetail.id,
+            productDetail.isbn,
+            productDetail.page,
+            productDetail.publisher,
+            productDetail.publishedDate,
+            productDetail.ebookAddress,
+            productDetail.storage,
+            from(productDetail).innerJoin(productAuthor).on(productDetail.id.eq(productAuthor.productDetail.id))
+                .innerJoin(author).on(productAuthor.author.authorId.eq(author.authorId))
+                .where(productDetail.id.eq(id))
+                .select(author.authorId),
+            from(productDetail).innerJoin(product).on(productDetail.product.id.eq(product.id))
+                .innerJoin(categoryProduct).on(product.id.eq(categoryProduct.product.id))
+                .innerJoin(category).on(categoryProduct.category.id.eq(category.id))
+                .where(productDetail.id.eq(id))
+                .select(category.id)
+        )).fetchOne();
   }
 }

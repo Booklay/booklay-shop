a4b1def3b66191d9e48a2b00010d3afe62f98add
최규태 <kuitae0725@gmail.com>
2023-01-28 22:42:07 +0900

    refactor : queryDsl 사용으로 개량

--- src/main/java/com/nhnacademy/booklay/server/repository/product/AuthorRepository.java
+++ src/main/java/com/nhnacademy/booklay/server/repository/product/AuthorRepository.java
@@ -5,7 +5,5 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-
-    <T> Page<T> findAllBy(Pageable pageable, Class<T> type);
+public interface AuthorRepository extends JpaRepository<Author, Long>, AuthorRepositoryCustom{
 }
---
+++ src/main/java/com/nhnacademy/booklay/server/repository/product/AuthorRepositoryImpl.java
@@ -0,0 +1,43 @@
+package com.nhnacademy.booklay.server.repository.product;
+
+import com.nhnacademy.booklay.server.dto.member.reponse.MemberForAuthorResponse;
+import com.nhnacademy.booklay.server.dto.product.author.response.RetrieveAuthorResponse;
+import com.nhnacademy.booklay.server.entity.Author;
+import com.nhnacademy.booklay.server.entity.QAuthor;
+import com.nhnacademy.booklay.server.entity.QMember;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.JPQLQuery;
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.springframework.data.support.PageableExecutionUtils;
+
+public class AuthorRepositoryImpl extends QuerydslRepositorySupport implements AuthorRepositoryCustom {
+
+  public AuthorRepositoryImpl() {
+    super(Author.class);
+  }
+
+  @Override
+  public Page<RetrieveAuthorResponse> findAllBy(Pageable pageable) {
+    QAuthor author = QAuthor.author;
+    QMember member = QMember.member;
+
+    List<RetrieveAuthorResponse> content = from(author)
+        .leftJoin(member).on(author.member.memberNo.eq(member.memberNo))
+        .select(Projections.constructor(RetrieveAuthorResponse.class,
+            author.authorId,
+            author.name,
+            Projections.constructor(MemberForAuthorResponse.class,
+                member.memberNo,
+                member.memberId)))
+        .offset(pageable.getOffset())
+        .limit(pageable.getPageSize())
+        .fetch();
+
+    JPQLQuery<Long> count = from(author)
+        .select(author.count());
+    return PageableExecutionUtils.getPage(content, pageable, count::fetchFirst);
+  }
+}

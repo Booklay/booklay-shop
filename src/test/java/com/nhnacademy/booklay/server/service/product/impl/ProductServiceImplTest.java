daa9b817984ec242f2a0c39e5d04b1aba7336451
최규태 <kuitae0725@gmail.com>
2023-01-29 16:31:14 +0900

    test : product service 테스트 작성

--- src/test/java/com/nhnacademy/booklay/server/service/product/impl/ProductServiceImplTest.java
+++ src/test/java/com/nhnacademy/booklay/server/service/product/impl/ProductServiceImplTest.java
@@ -1,8 +1,21 @@
 package com.nhnacademy.booklay.server.service.product.impl;
 
 
-import com.nhnacademy.booklay.server.repository.category.CategoryRepository;
+import static org.assertj.core.api.Assertions.as;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+import com.nhnacademy.booklay.server.dto.product.request.CreateUpdateProductBookRequest;
+import com.nhnacademy.booklay.server.dto.product.request.CreateUpdateProductSubscribeRequest;
+import com.nhnacademy.booklay.server.dto.product.response.RetrieveProductBookResponse;
+import com.nhnacademy.booklay.server.dto.product.response.RetrieveProductSubscribeResponse;
+import com.nhnacademy.booklay.server.dummy.DummyCart;
+import com.nhnacademy.booklay.server.entity.Product;
+import com.nhnacademy.booklay.server.entity.ProductDetail;
+import com.nhnacademy.booklay.server.entity.Subscribe;
 import com.nhnacademy.booklay.server.repository.ImageRepository;
+import com.nhnacademy.booklay.server.repository.category.CategoryRepository;
 import com.nhnacademy.booklay.server.repository.product.AuthorRepository;
 import com.nhnacademy.booklay.server.repository.product.CategoryProductRepository;
 import com.nhnacademy.booklay.server.repository.product.ProductAuthorRepository;
@@ -10,11 +23,19 @@ import com.nhnacademy.booklay.server.repository.product.ProductDetailRepository;
 import com.nhnacademy.booklay.server.repository.product.ProductRepository;
 import com.nhnacademy.booklay.server.repository.product.ProductTagRepository;
 import com.nhnacademy.booklay.server.repository.product.SubscribeRepository;
+import com.nhnacademy.booklay.server.service.storage.impl.FileServiceImpl;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
 @Slf4j
 @ExtendWith(MockitoExtension.class)
@@ -41,4 +62,115 @@ public class ProductServiceImplTest {
   private ImageRepository imageRepository;
   @Mock
   private ProductTagRepository productTagRepository;
+  @Mock
+  private FileServiceImpl fileService;
+
+  Product productBook;
+  ProductDetail productDetail;
+  CreateUpdateProductBookRequest productRequest;
+  Product productSubscribe;
+  Subscribe subscribe;
+  CreateUpdateProductSubscribeRequest subscribeRequest;
+
+
+  @BeforeEach
+  void setUp() {
+    productRequest = DummyCart.getDummyProductBookDto();
+    productBook = DummyCart.getDummyProduct(productRequest);
+    productDetail = DummyCart.getDummyProductDetail(productRequest);
+
+    subscribeRequest = DummyCart.getDummyProductSubscribeDto();
+    productSubscribe = DummyCart.getDummyProduct(subscribeRequest);
+    subscribe = DummyCart.getDummySubscribe(subscribeRequest);
+  }
+
+  @Test
+  void createBookProduct_success() throws Exception {
+    ReflectionTestUtils.setField(productBook, "id", 1L);
+    ReflectionTestUtils.setField(productDetail, "id", 1L);
+
+    given(productRepository.save(any())).willReturn(productBook);
+    for (int i = 0; i < productRequest.getCategoryIds().size(); i++) {
+      given(categoryRepository.findById(productRequest.getCategoryIds().get(i))).willReturn(
+          Optional.ofNullable(DummyCart.getDummyCategory()));
+    }
+
+    given(productDetailRepository.save(any())).willReturn(productDetail);
+    for (int i = 0; i < productRequest.getAuthorIds().size(); i++) {
+      given(authorRepository.findById(productRequest.getAuthorIds().get(i))).willReturn(
+          Optional.of(DummyCart.getDummyAuthor()));
+    }
+
+    //when
+    Long result = productService.createBookProduct(productRequest);
+
+    //then
+    assertThat(result).isEqualTo(productBook.getId());
+  }
+
+  @Test
+  void createSubscribeProduct_success() throws Exception {
+    ReflectionTestUtils.setField(productSubscribe, "id", 1L);
+    ReflectionTestUtils.setField(subscribe, "id", 1L);
+
+    given(productRepository.save(any())).willReturn(productSubscribe);
+    for (int i = 0; i < subscribeRequest.getCategoryIds().size(); i++) {
+      given(categoryRepository.findById(subscribeRequest.getCategoryIds().get(i))).willReturn(
+          Optional.ofNullable(DummyCart.getDummyCategory()));
+    }
+
+    given(subscribeRepository.save(any())).willReturn(subscribe);
+
+    //when
+    Long result = productService.createSubscribeProduct(subscribeRequest);
+
+    //then
+    assertThat(result).isEqualTo(productSubscribe.getId());
+  }
+
+
+  @Test
+  void testRetrieveBookData_success(){
+    Long targetId = 1L;
+    RetrieveProductBookResponse response = new RetrieveProductBookResponse();
+    List<Long> authorIds = new ArrayList<>();
+    authorIds.add(1L);
+    List<Long> categoryIds = new ArrayList<>();
+    categoryIds.add(1L);
+
+    given(productRepository.findProductBookDataByProductId(targetId)).willReturn(response);
+    given(productDetailRepository.findAuthorIdsByProductDetailId(response.getProductDetailId())).willReturn(authorIds);
+    given(productRepository.findCategoryIdsByProductId(response.getProductId())).willReturn(categoryIds);
+
+    RetrieveProductBookResponse result = productService.retrieveBookData(targetId);
+
+    assertThat(result.getCategoryIds()).isEqualTo(categoryIds);
+    assertThat(result.getAuthorIds()).isEqualTo(authorIds);
+  }
+
+  @Test
+  void testRetrieveSubscribeData(){
+    Long targetId = 1L;
+    RetrieveProductSubscribeResponse response = new RetrieveProductSubscribeResponse();
+    List<Long> categoryIds = new ArrayList<>();
+    categoryIds.add(1L);
+
+    given(productRepository.findProductSubscribeDataByProductId(targetId)).willReturn(response);
+    given(productRepository.findCategoryIdsByProductId(response.getProductId())).willReturn(categoryIds);
+
+    RetrieveProductSubscribeResponse result = productService.retrieveSubscribeData(targetId);
+
+    assertThat(result.getCategoryIds()).isEqualTo(categoryIds);
+  }
+
+  @Test
+  void testSoftDelete_success(){
+    given(productRepository.findById(productBook.getId())).willReturn(
+        Optional.ofNullable(productBook));
+
+    productService.softDelete(productBook.getId());
+
+    assertThat(productBook.isDeleted()).isTrue();
+  }
+
 }

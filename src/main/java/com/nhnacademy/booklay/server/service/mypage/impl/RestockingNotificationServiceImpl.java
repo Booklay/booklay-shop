c4a5ada7df5e52023b0001562c94fe7fe9abf6a0
최규태 <kuitae0725@gmail.com>
2023-02-03 15:50:26 +0900

    test : 재입고 알림 서비스 테스트 작성

--- src/main/java/com/nhnacademy/booklay/server/service/mypage/impl/RestockingNotificationServiceImpl.java
+++ src/main/java/com/nhnacademy/booklay/server/service/mypage/impl/RestockingNotificationServiceImpl.java
@@ -14,6 +14,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * @author 최규태
+ */
+
 @Slf4j
 @Service
 @Transactional

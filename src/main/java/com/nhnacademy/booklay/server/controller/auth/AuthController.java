48561e7aa0815cad84778200e49ba46f8cb7d0d1
Com-Sun <gnlslfflffl@gmail.com>
2023-02-04 15:22:27 +0900

    fix: typo

--- src/main/java/com/nhnacademy/booklay/server/controller/auth/AuthController.java
+++ src/main/java/com/nhnacademy/booklay/server/controller/auth/AuthController.java
@@ -1,9 +1,6 @@
 package com.nhnacademy.booklay.server.controller.auth;
 
 import com.nhnacademy.booklay.server.dto.member.reponse.MemberLoginResponse;
-import com.nhnacademy.booklay.server.dto.member.reponse.MemberRetrieveResponse;
-import com.nhnacademy.booklay.server.exception.member.MemberNotFoundException;
-import com.nhnacademy.booklay.server.exception.service.NotFoundException;
 import com.nhnacademy.booklay.server.service.member.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;

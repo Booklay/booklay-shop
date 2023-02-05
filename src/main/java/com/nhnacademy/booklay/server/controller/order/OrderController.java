e2b7bd5b959fbbb08e879ef38a0c7a5e84710c51
joonhoo oh <ojh789ojh@naver.com>
2023-02-05 21:43:09 +0900

    CategoryProductService 를 통해서 상품번호를 통해 카테고리 리스트 반환

--- src/main/java/com/nhnacademy/booklay/server/controller/order/OrderController.java
+++ src/main/java/com/nhnacademy/booklay/server/controller/order/OrderController.java
@@ -1,11 +1,13 @@
 package com.nhnacademy.booklay.server.controller.order;
 
 import com.nhnacademy.booklay.server.dto.cart.CartRetrieveResponse;
+import com.nhnacademy.booklay.server.service.category.CategoryProductService;
 import com.nhnacademy.booklay.server.service.product.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,13 +21,17 @@ import java.util.stream.Collectors;
 @RequestMapping("order")
 public class OrderController {
     private final ProductService productService;
+    private final CategoryProductService categoryProductService;
     @GetMapping("products")
     public ResponseEntity<List<CartRetrieveResponse>> getProductDataByProductList(
-            @RequestParam("productNoList") List<Long> productNoList) {
+        @RequestParam("productNoList") List<Long> productNoList) {
+        MultiValueMap<Long, Long> multiValueMap =
+            categoryProductService.retrieveCategoryIdListMultiValueMapByProductIdList(productNoList);
         List<CartRetrieveResponse> cartList = productService.retrieveProductListByProductNoList(productNoList).stream()
-                .map(product -> new CartRetrieveResponse(product.getId(), product.getTitle(), product.getPrice(), 0)).collect(Collectors.toList());
+            .map(product -> new CartRetrieveResponse(product.getId(), product.getTitle(), product.getPrice(), 0,
+                multiValueMap.get(product.getId()))).collect(Collectors.toList());
         return ResponseEntity.status(HttpStatus.OK)
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(cartList);
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(cartList);
     }
 }

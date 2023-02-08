b3063e476f857883cf7ff9f961a2902e364e6dd4
opgumo <opgumo@naver.com>
2023-02-08 17:57:45 +0900

    feat: 회원의 쿠폰 발급 구현

--- src/main/java/com/nhnacademy/booklay/server/controller/coupon/CouponZoneController.java
+++ src/main/java/com/nhnacademy/booklay/server/controller/coupon/CouponZoneController.java
@@ -1,16 +1,25 @@
 package com.nhnacademy.booklay.server.controller.coupon;
 
-import com.nhnacademy.booklay.server.dto.coupon.message.CouponIssueRequestMessage;
+import com.nhnacademy.booklay.server.dto.coupon.request.CouponIssueRequest;
+import com.nhnacademy.booklay.server.dto.coupon.response.CouponIssueResponse;
+import com.nhnacademy.booklay.server.dto.coupon.response.CouponMemberResponse;
 import com.nhnacademy.booklay.server.service.kafka.CouponZoneIssueService;
 import com.nhnacademy.booklay.server.service.member.GetMemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 회원의 쿠폰 발급 요청을 처리합니다.
+ * @author 김승혜
+ */
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/member/coupon-zone")
@@ -20,14 +29,30 @@ public class CouponZoneController {
     private final GetMemberService memberService;
     private final CouponZoneIssueService issueService;
 
-    @PutMapping
-    public ResponseEntity<Void> issueCoupon(@RequestBody CouponIssueRequestMessage request) {
-        // TODO requestBody로 객체 받고, OK
-        //  member 검증, mq에 저장, OK
-        //  소켓 열기(randomValue),
-        //  레디스에 저장(memberNo-couponNo : randomValue).
+    /**
+     *
+     * @param request
+     */
+    @PostMapping
+    public ResponseEntity<CouponIssueResponse> issueCoupon(@RequestBody CouponIssueRequest request) {
+        log.info(request.getCouponId() + " :: 받았습니다.");
+
         memberService.getMemberNo(request.getMemberId());
-        issueService.requestIssueCoupon(request);
-        return null;
+
+        String requestId = issueService.requestIssueCoupon(request);
+        CouponIssueResponse response = new CouponIssueResponse(requestId);
+
+        log.info(requestId);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(response);
+    }
+
+    @GetMapping("/{requestId}")
+    public ResponseEntity<CouponMemberResponse> responseIssueCoupon(@PathVariable String requestId) {
+        CouponMemberResponse response = issueService.getResponse(requestId);
+        log.info("메세지 가져가유~");
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(response);
     }
 }

1b0a5241cdbf8a8efdded7d76e1a1ad8b3cece45
최규태 <kuitae0725@gmail.com>
2023-02-09 11:00:28 +0900

    feat : 오브젝트 스토리지의 사진 부분 개선

--- src/main/java/com/nhnacademy/booklay/server/controller/storage/StorageController.java
+++ src/main/java/com/nhnacademy/booklay/server/controller/storage/StorageController.java
@@ -1,12 +1,8 @@
 package com.nhnacademy.booklay.server.controller.storage;
 
-import com.nhnacademy.booklay.server.dto.stroage.request.FileResolveRequest;
 import com.nhnacademy.booklay.server.entity.ObjectFile;
 import com.nhnacademy.booklay.server.service.storage.FileService;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -24,42 +20,49 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/storage")
 public class StorageController {
 
-    private final FileService fileService;
+  private final FileService fileService;
 
-    public StorageController(FileService fileService) {
-        this.fileService = fileService;
-    }
+  public StorageController(FileService fileService) {
+    this.fileService = fileService;
+  }
 
-    /**
-     * Object Storage Upload.
-     *
-     * @param file html input type = file 등으로 첨부파일 선택.
-     * @return DB에 저장된 ID, 디렉토리를 포함한 주소, 파일 이름.
-     * @throws IOException .
-     */
+  /**
+   * Object Storage Upload.
+   *
+   * @param file html input type = file 등으로 첨부파일 선택.
+   * @return DB에 저장된 ID, 디렉토리를 포함한 주소, 파일 이름.
+   * @throws IOException .
+   */
 
-    @PostMapping
-    public ResponseEntity<ObjectFile> uploadFile(@RequestPart final MultipartFile file)
-        throws IOException {
-        ObjectFile objectFile = fileService.uploadFile(file);
+  @PostMapping
+  public ResponseEntity<ObjectFile> uploadFile(@RequestPart final MultipartFile file)
+      throws IOException {
+    ObjectFile objectFile = fileService.uploadFile(file);
 
-        return ResponseEntity.status(HttpStatus.CREATED)
-            .contentType(MediaType.APPLICATION_JSON)
-            .body(objectFile);
-    }
+    return ResponseEntity.status(HttpStatus.CREATED)
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(objectFile);
+  }
 
-    /**
-     * Object Storage Download.
-     *
-     * @param fileId 파일이 저장된 주소를 가지고 있는 튜플의 ID
-     * @return 저장된 파일 위치
-     * @throws IOException .
-     */
+  /**
+   * Object Storage Download.
+   *
+   * @param fileId 파일이 저장된 주소를 가지고 있는 튜플의 ID
+   * @return 저장된 파일 위치
+   * @throws IOException .
+   */
 
-    @GetMapping("/{fileId}")
-    public ResponseEntity<byte[]> downloadFile(@PathVariable("fileId") final Long fileId)
-        throws IOException {
-        return fileService.downloadFile(fileId);
-    }
+  @GetMapping("/{fileId}")
+  public ResponseEntity<String> downloadUrl(@PathVariable("fileId") final Long fileId)
+      throws IOException {
+    return ResponseEntity.status(HttpStatus.OK)
+        .body(fileService.downloadUrl(fileId));
+  }
+
+  //    @GetMapping("/ebook/{fileId}")
+  public ResponseEntity<byte[]> downloadEBook(@PathVariable("fileId") final Long fileId)
+      throws IOException {
+    return fileService.downloadFile(fileId);
+  }
 
 }

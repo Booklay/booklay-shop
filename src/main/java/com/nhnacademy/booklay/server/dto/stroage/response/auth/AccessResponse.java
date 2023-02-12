b3bb9301872dfa31083ca154e28ad13931f3662f
devel0624 <devel0624@gmail.com>
2023-02-12 14:46:08 +0900

    test: StorageController, service 테스트 추가

--- src/main/java/com/nhnacademy/booklay/server/dto/stroage/response/auth/AccessResponse.java
+++ src/main/java/com/nhnacademy/booklay/server/dto/stroage/response/auth/AccessResponse.java
@@ -2,9 +2,11 @@ package com.nhnacademy.booklay.server.dto.stroage.response.auth;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @NoArgsConstructor
 @Getter
+@ToString
 public class AccessResponse {
     private Access access;
 

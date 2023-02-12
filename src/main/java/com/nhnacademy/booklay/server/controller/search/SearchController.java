24e15dc711d168997de4e25677c317d6bc6c54ed
devel0624 <devel0624@gmail.com>
2023-02-12 17:09:46 +0900

    feat: 최근 등록 상품 추천과 태그 작가 검색 기능 추가

--- src/main/java/com/nhnacademy/booklay/server/controller/search/SearchController.java
+++ src/main/java/com/nhnacademy/booklay/server/controller/search/SearchController.java
@@ -5,6 +5,7 @@ import com.nhnacademy.booklay.server.dto.search.request.SearchKeywordsRequest;
 import com.nhnacademy.booklay.server.dto.search.response.SearchPageResponse;
 import com.nhnacademy.booklay.server.dto.search.response.SearchProductResponse;
 import com.nhnacademy.booklay.server.service.search.SearchService;
+import java.util.List;
 import javax.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
@@ -34,6 +35,15 @@ public class SearchController {
 
         return ResponseEntity.status(HttpStatus.OK)
             .body(pageResponse);
+    }
+
+    @GetMapping("/products/latest")
+    public ResponseEntity<List<SearchProductResponse>> searchRecently() {
+
+        List<SearchProductResponse> latestProducts = searchService.getLatestProducts();
+
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(latestProducts);
     }
 
     @PostMapping("/products")

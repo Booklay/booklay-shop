96e7bd3b27fe70911f857423eb96d7f378c7075c
devel0624 <devel0624@gmail.com>
2023-02-13 08:49:47 +0900

    fix: ElasticSearchConfig try catch 문 추가

--- src/main/java/com/nhnacademy/booklay/server/config/ElasticSearchConfig.java
+++ src/main/java/com/nhnacademy/booklay/server/config/ElasticSearchConfig.java
@@ -1,5 +1,7 @@
 package com.nhnacademy.booklay.server.config;
 
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +10,7 @@ import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
+@Slf4j
 @Configuration
 @EnableElasticsearchRepositories // elasticsearch repository 허용
 public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
@@ -17,9 +20,19 @@ public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
 
     @Override
     public RestHighLevelClient elasticsearchClient() {
+
         ClientConfiguration clientConfiguration = ClientConfiguration.builder()
             .connectedTo(hostAndPort)
             .build();
-        return RestClients.create(clientConfiguration).rest();
+
+
+        try (RestClients.ElasticsearchRestClient client = RestClients.create(clientConfiguration)) {
+
+            return client.rest();
+
+        } catch (IOException e) {
+            log.error(e.getMessage());
+            throw new RuntimeException(e.getMessage());
+        }
     }
 }

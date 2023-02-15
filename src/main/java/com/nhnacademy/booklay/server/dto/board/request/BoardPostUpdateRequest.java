bb65a08f0ba4f9f346585adad77d5ac398cb76ef
최규태 <kuitae0725@gmail.com>
2023-02-15 15:35:35 +0900

    feat : 게시물 수정 DTO

---
+++ src/main/java/com/nhnacademy/booklay/server/dto/board/request/BoardPostUpdateRequest.java
@@ -0,0 +1,16 @@
+package com.nhnacademy.booklay.server.dto.board.request;
+
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class BoardPostUpdateRequest {
+
+  @NotNull
+  private String title;
+  @NotNull
+  private String content;
+  private Boolean viewPublic;
+}

98c18aa633d1d4e5e56eaa53b5c91cd27b95f40b
devel0624 <devel0624@gmail.com>
2023-02-16 21:27:05 +0900

    fix: 상품 노출 버그 수정

--- src/main/java/com/nhnacademy/booklay/server/repository/product/ProductRepository.java
+++ src/main/java/com/nhnacademy/booklay/server/repository/product/ProductRepository.java
@@ -1,11 +1,15 @@
 package com.nhnacademy.booklay.server.repository.product;
 
 import com.nhnacademy.booklay.server.entity.Product;
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long>, ProductRepositoryCustom {
 
+    List<Product> findTop8ByIsDeletedOrderByCreatedAtDesc(boolean isDeleted);
+
+    List<Product> findByIsDeleted(boolean isDeleted, Pageable pageable);
     <T> Page<T> findAllBy(Pageable pageable, Class<T> type);
 }

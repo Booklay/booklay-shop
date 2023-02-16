3e88b54492d3622116eb7990e7e8351eeaf8350e
yangrystal <tmddk512@naver.com>
2023-02-16 16:54:28 +0900

    fix: pointCoupon 전환로직 삭제

--- src/main/java/com/nhnacademy/booklay/server/dto/member/request/PointHistoryCreateRequest.java
+++ src/main/java/com/nhnacademy/booklay/server/dto/member/request/PointHistoryCreateRequest.java
@@ -3,6 +3,7 @@ package com.nhnacademy.booklay.server.dto.member.request;
 import com.nhnacademy.booklay.server.entity.Member;
 import com.nhnacademy.booklay.server.entity.PointHistory;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,10 +14,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PointHistoryCreateRequest {
-    @NotBlank
+    @NotNull
     private Long memberNo;
 
-    @NotBlank
+    @NotNull
     private Integer point;
 
     @NotBlank

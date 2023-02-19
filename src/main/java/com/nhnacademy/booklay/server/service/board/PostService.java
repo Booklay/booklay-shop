82346c492aae1e98c593c35a3bc956cc8fcc5a90
최규태 <kuitae0725@gmail.com>
2023-02-19 15:06:40 +0900

    feat : 관리자 게시판에 최근 공지사항 5개 제한 출력기능 추가

--- src/main/java/com/nhnacademy/booklay/server/service/board/PostService.java
+++ src/main/java/com/nhnacademy/booklay/server/service/board/PostService.java
@@ -1,25 +1,21 @@
 package com.nhnacademy.booklay.server.service.board;
 
+import com.nhnacademy.booklay.server.dto.PageResponse;
 import com.nhnacademy.booklay.server.dto.board.request.BoardPostCreateRequest;
 import com.nhnacademy.booklay.server.dto.board.request.BoardPostUpdateRequest;
 import com.nhnacademy.booklay.server.dto.board.response.PostResponse;
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface PostService {
 
   Long createPost(BoardPostCreateRequest request);
-
   Page<PostResponse> retrieveProductQNA(Long productId, Pageable pageable);
-
   Page<PostResponse> retrieveNotice(Pageable pageable);
-
   PostResponse retrievePostById(Long postId);
-
-
   Long updatePost(BoardPostUpdateRequest request);
-
   void deletePost(Long memberId, Long postId);
-
   Long updateConfirmAnswer(Long postId);
+  List<PostResponse> retrieveNoticeList(Integer pageLimit);
 }

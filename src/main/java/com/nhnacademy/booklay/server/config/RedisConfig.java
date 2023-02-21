ddb963c25d4babf825283b17d3ceeb872ef0af67
devel0624 <devel0624@gmail.com>
2023-02-21 13:50:50 +0900

    docs: 로그백 설정 범위 수정

--- src/main/java/com/nhnacademy/booklay/server/config/RedisConfig.java
+++ src/main/java/com/nhnacademy/booklay/server/config/RedisConfig.java
@@ -12,7 +12,6 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-//@Profile("redis")
 @Configuration
 public class RedisConfig {
 

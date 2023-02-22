dd97b80c79a06f8be9278af1bae0ba97d6473b95
최규태 <kuitae0725@gmail.com>
2023-02-22 21:33:53 +0900

    test : search controller test 작성

--- src/test/java/com/nhnacademy/booklay/server/controller/search/SearchControllerTest.java
+++ src/test/java/com/nhnacademy/booklay/server/controller/search/SearchControllerTest.java
@@ -1,27 +1,49 @@
 package com.nhnacademy.booklay.server.controller.search;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nhnacademy.booklay.server.controller.admin.product.TagAdminController;
 import com.nhnacademy.booklay.server.dto.search.request.SearchIdRequest;
 import com.nhnacademy.booklay.server.dto.search.request.SearchKeywordsRequest;
+import com.nhnacademy.booklay.server.dto.search.response.SearchProductResponse;
 import com.nhnacademy.booklay.server.service.search.SearchService;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.context.WebApplicationContext;
 
-
-@WebMvcTest(SearchController.class)
+@AutoConfigureRestDocs(uriScheme = "https", uriHost = "docs.api.com")
+@ExtendWith(RestDocumentationExtension.class)
 @MockBean(JpaMetamodelMappingContext.class)
+@WebMvcTest(SearchController.class)
 class SearchControllerTest {
 
     @MockBean
@@ -41,7 +63,17 @@ class SearchControllerTest {
 
 
     @BeforeEach
-    void setUp() {
+    void setUp(WebApplicationContext webApplicationContext,
+        RestDocumentationContextProvider restDocumentation) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+            .apply(documentationConfiguration(restDocumentation))
+            .alwaysDo(print())
+            .alwaysDo(document("search/{methodName}",
+                    preprocessRequest(prettyPrint()),
+                    preprocessResponse(prettyPrint())
+                )
+            )
+            .build();
 
         mapper = new ObjectMapper();
         request = new SearchKeywordsRequest();
@@ -56,6 +88,7 @@ class SearchControllerTest {
             .andDo(print())
             .andReturn();
 
+        then(searchService).should(times(1)).getAllProducts(any());
     }
 
     @Test
@@ -71,6 +104,7 @@ class SearchControllerTest {
             .andDo(print())
             .andReturn();
 
+        then(searchService).should(times(1)).searchProductsByKeywords(any(), any());
     }
 
     @Test
@@ -88,6 +122,7 @@ class SearchControllerTest {
             .andDo(print())
             .andReturn();
 
+        then(searchService).should(times(1)).searchProductsByCategory(any(), any());
     }
 
     @Test
@@ -98,5 +133,18 @@ class SearchControllerTest {
             .andDo(print())
             .andReturn();
 
+        then(searchService).should(times(1)).saveAllDocuments();
+    }
+
+    @Test
+    void searchRecently() throws Exception {
+        when(searchService.getLatestProducts()).thenReturn(List.of());
+
+        mockMvc.perform(get(URI_PREFIX + "/products/latest"))
+            .andExpect(status().isOk())
+            .andDo(print())
+            .andReturn();
+
+        then(searchService).should(times(1)).getLatestProducts();
     }
 }

a54bd9778cf81a00ced4a0c5feb0e0ed3c4fc238
최규태 <kuitae0725@gmail.com>
2023-02-22 09:56:51 +0900

    test : author admin controller test 작성

--- src/test/java/com/nhnacademy/booklay/server/controller/admin/product/AuthorAdminControllerTest.java
+++ src/test/java/com/nhnacademy/booklay/server/controller/admin/product/AuthorAdminControllerTest.java
@@ -1,13 +1,21 @@
 package com.nhnacademy.booklay.server.controller.admin.product;
 
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nhnacademy.booklay.server.controller.admin.category.CategoryAdminController;
 import com.nhnacademy.booklay.server.dto.product.DeleteIdRequest;
 import com.nhnacademy.booklay.server.dto.product.author.request.CreateAuthorRequest;
 import com.nhnacademy.booklay.server.dto.product.author.request.UpdateAuthorRequest;
@@ -17,24 +25,33 @@ import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+
+@AutoConfigureRestDocs(uriScheme = "https", uriHost = "docs.api.com")
+@ExtendWith(RestDocumentationExtension.class)
 @WebMvcTest(AuthorAdminController.class)
 @MockBean(JpaMetamodelMappingContext.class)
 class AuthorAdminControllerTest {
 
   @MockBean
   AuthorService authorService;
-
   @Autowired
   AuthorAdminController authorAdminController;
   @Autowired
@@ -44,8 +61,21 @@ class AuthorAdminControllerTest {
   Long authorNo;
   UpdateAuthorRequest updateAuthorRequest;
 
+  private final String URI_PRE_FIX = "/admin/author";
+
   @BeforeEach
-  void setUp() {
+  void setUp(WebApplicationContext webApplicationContext,
+      RestDocumentationContextProvider restDocumentation) {
+    this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+        .apply(documentationConfiguration(restDocumentation))
+        .alwaysDo(print())
+        .alwaysDo(document("admin/author/{methodName}",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint())
+            )
+        )
+        .build();
+
     objectMapper = new ObjectMapper();
     author = new RetrieveAuthorResponse(1L, "test");
     authorNo = 1L;
@@ -55,14 +85,16 @@ class AuthorAdminControllerTest {
   @Test
   void authorPage() throws Exception {
     //given
-    Pageable pageable = Pageable.ofSize(10);
-    List<RetrieveAuthorResponse> content = new ArrayList<>();
-    content.add(author);
-    Page<RetrieveAuthorResponse> responses = new PageImpl(content, pageable, 1);
-    given(authorService.retrieveAllAuthor(pageable)).willReturn(responses);
+    Pageable pageable = PageRequest.of(0,20);
+    Page<RetrieveAuthorResponse> page = new PageImpl(List.of(), pageable, 0);
 
-    ResultActions result = mockMvc.perform(get("/admin/author").accept(MediaType.APPLICATION_JSON));
-//    result.andExpect(status().isOk());
+    when(authorService.retrieveAllAuthor(pageable)).thenReturn(page);
+
+    //when
+    mockMvc.perform(get(URI_PRE_FIX).accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andDo(print())
+        .andReturn();
   }
 
   @Test
@@ -71,7 +103,7 @@ class AuthorAdminControllerTest {
     given(authorService.retrieveAuthorForUpdate(authorNo)).willReturn(author);
 
     //when
-    ResultActions result = mockMvc.perform(get("/admin/author/" + authorNo)
+    ResultActions result = mockMvc.perform(get(URI_PRE_FIX + authorNo)
         .accept(MediaType.APPLICATION_JSON));
 
     //then
@@ -82,7 +114,7 @@ class AuthorAdminControllerTest {
   void authorRegister() throws Exception {
     CreateAuthorRequest createAuthorRequest = new CreateAuthorRequest("create");
 
-    mockMvc.perform(post("/admin/author")
+    mockMvc.perform(post(URI_PRE_FIX)
             .content(objectMapper.writeValueAsString(createAuthorRequest))
             .contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isCreated());
@@ -91,7 +123,7 @@ class AuthorAdminControllerTest {
   @Test
   void authorUpdate() throws Exception {
     //then
-    mockMvc.perform(put("/admin/author")
+    mockMvc.perform(put(URI_PRE_FIX)
             .content(objectMapper.writeValueAsString(updateAuthorRequest))
             .contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isAccepted());
@@ -101,7 +133,7 @@ class AuthorAdminControllerTest {
   void authorDelete() throws Exception {
     DeleteIdRequest deleteIdRequest = new DeleteIdRequest(authorNo);
 
-    mockMvc.perform(delete("/admin/author")
+    mockMvc.perform(delete(URI_PRE_FIX)
             .content(objectMapper.writeValueAsString(deleteIdRequest))
             .contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isOk());

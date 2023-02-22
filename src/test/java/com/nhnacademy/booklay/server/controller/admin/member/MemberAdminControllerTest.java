6ac4c089e424613cd14617c9003dbde28ceb6362
yangrystal <tmddk512@naver.com>
2023-02-22 23:30:33 +0900

    fix: code smell 제거

--- src/test/java/com/nhnacademy/booklay/server/controller/admin/member/MemberAdminControllerTest.java
+++ src/test/java/com/nhnacademy/booklay/server/controller/admin/member/MemberAdminControllerTest.java
@@ -26,11 +26,9 @@ import com.nhnacademy.booklay.server.dto.member.response.MemberGradeRetrieveResp
 import com.nhnacademy.booklay.server.dto.member.response.MemberRetrieveResponse;
 import com.nhnacademy.booklay.server.dummy.Dummy;
 import com.nhnacademy.booklay.server.entity.Member;
-import com.nhnacademy.booklay.server.exception.member.MemberNotFoundException;
 import com.nhnacademy.booklay.server.service.member.MemberService;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -125,7 +123,6 @@ class MemberAdminControllerTest {
     }
 
     @Test
-    @DisplayName("관리자의 단일회원 조회 성공 테스트")
     void testRetrieveMemberSuccess() throws Exception {
         //mocking
         when(memberService.retrieveMember(member.getMemberNo())).thenReturn(memberRetrieveResponse);
@@ -134,21 +131,6 @@ class MemberAdminControllerTest {
         mockMvc.perform(
                 get(URI_PREFIX + "/" + member.getMemberNo()).accept(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk()).andDo(print()).andReturn();
-    }
-
-
-    @Disabled
-    @Test
-    @DisplayName("관리자의 단일회원 조회 실패 테스트")
-    void testRetrieveMemberTest_ifNotExistMemberId() throws Exception {
-        //mocking
-        when(memberService.retrieveMember(member.getMemberNo())).thenThrow(
-            MemberNotFoundException.class);
-
-        //then
-        mockMvc.perform(
-                get(URI_PREFIX + "/" + member.getMemberNo()).accept(MediaType.APPLICATION_JSON))
-            .andExpect(status().isBadRequest()).andDo(print()).andReturn();
     }
 
     @Test

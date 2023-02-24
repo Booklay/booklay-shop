5c6bf7b7d34cf0757355d2738153b5b55c0d5905
opgumo <opgumo@naver.com>
2023-02-24 16:53:13 +0900

    refactor: coupon unusing dto 삭제

--- src/main/java/com/nhnacademy/booklay/server/repository/coupon/CouponTypeRepository.java
+++ src/main/java/com/nhnacademy/booklay/server/repository/coupon/CouponTypeRepository.java
@@ -1,11 +1,7 @@
 package com.nhnacademy.booklay.server.repository.coupon;
 
-import com.nhnacademy.booklay.server.dto.coupon.CouponTypeRetrieveResponse;
 import com.nhnacademy.booklay.server.entity.CouponType;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CouponTypeRepository extends JpaRepository<CouponType, Long> {
-    Page<CouponTypeRetrieveResponse> findAllBy(Pageable pageable);
 }

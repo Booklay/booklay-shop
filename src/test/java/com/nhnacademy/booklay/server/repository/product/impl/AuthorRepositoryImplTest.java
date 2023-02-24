a413ba67385767a574f0a2e70b517ee4754408f9
최규태 <kuitae0725@gmail.com>
2023-02-24 17:56:58 +0900

    fix : author repo test edit

--- src/test/java/com/nhnacademy/booklay/server/repository/product/impl/AuthorRepositoryImplTest.java
+++ src/test/java/com/nhnacademy/booklay/server/repository/product/impl/AuthorRepositoryImplTest.java
@@ -8,6 +8,7 @@ import com.nhnacademy.booklay.server.entity.Author;
 import com.nhnacademy.booklay.server.repository.product.AuthorRepository;
 import javax.persistence.EntityManager;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;

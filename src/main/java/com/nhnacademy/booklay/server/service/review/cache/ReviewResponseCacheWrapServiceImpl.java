2944d4482cb61a64db487b2ee005f6937edfe78d
joonhoo oh <ojh789ojh@naver.com>
2023-02-25 23:21:57 +0900

    review캐시 수정

--- src/main/java/com/nhnacademy/booklay/server/service/review/cache/ReviewResponseCacheWrapServiceImpl.java
+++ src/main/java/com/nhnacademy/booklay/server/service/review/cache/ReviewResponseCacheWrapServiceImpl.java
@@ -2,122 +2,43 @@ package com.nhnacademy.booklay.server.service.review.cache;
 
 import static com.nhnacademy.booklay.server.utils.CacheKeyName.REVIEW_PAGE_KEY_NAME;
 
-import com.nhnacademy.booklay.server.dto.review.cache.ReviewResponseWrapDto;
+import com.nhnacademy.booklay.server.dto.product.cache.ObjectWrapDto;
 import com.nhnacademy.booklay.server.dto.review.response.RetrieveReviewResponse;
 import com.nhnacademy.booklay.server.dto.search.response.SearchPageResponse;
 import com.nhnacademy.booklay.server.service.RedisCacheService;
 import com.nhnacademy.booklay.server.service.review.ReviewService;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import lombok.RequiredArgsConstructor;
+import com.nhnacademy.booklay.server.utils.CacheServiceExtend;
 import org.springframework.data.domain.Pageable;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
-public class ReviewResponseCacheWrapServiceImpl implements ReviewResponsePageCacheWrapService {
-    private final RedisCacheService redisCacheService;
+public class ReviewResponseCacheWrapServiceImpl extends CacheServiceExtend<SearchPageResponse<RetrieveReviewResponse>>
+    implements ReviewResponsePageCacheWrapService {
     private final ReviewService reviewService;
-    private ReviewResponseWrapDto first;
-    private ReviewResponseWrapDto last;
-    private final Map<Long, ReviewResponseWrapDto> reviewResponseWrapDtoHashMap = new HashMap<>();
-    //맵에 넣기 위한 대기열
-    private final List<ReviewResponseWrapDto> tempList = Collections.synchronizedList(new LinkedList<>());
-    private static final int MAX_CACHE_NUM = 200;
 
+    public ReviewResponseCacheWrapServiceImpl(RedisCacheService redisCacheService,
+                                              ReviewService reviewService) {
+        super(redisCacheService, REVIEW_PAGE_KEY_NAME);
+        this.reviewService = reviewService;
+    }
     @Override
     public SearchPageResponse<RetrieveReviewResponse> cacheRetrievePostResponsePage(Long productId,
                                                                                     Pageable pageable){
         if (pageable.getPageNumber() == 0){
-            ReviewResponseWrapDto wrapDto = reviewResponseWrapDtoHashMap.get(productId);
-            SearchPageResponse<RetrieveReviewResponse>
-                response = wrapDto==null?
-                reviewService.retrieveReviewListByProductId(productId, pageable):wrapDto.getData();
-            ReviewResponseWrapDto postResponseWrapDto = new ReviewResponseWrapDto();
-            postResponseWrapDto.setData(response);
-            postResponseWrapDto.setProductId(productId);
-            tempList.add(postResponseWrapDto);
-            return response;
+            ObjectWrapDto<SearchPageResponse<RetrieveReviewResponse>> wrapDto = wrapDtoMap.get(productId);
+            if (wrapDto == null){
+                SearchPageResponse<RetrieveReviewResponse> response =
+                    reviewService.retrieveReviewListByProductId(productId, pageable);
+                wrapDto = new ObjectWrapDto<>();
+                wrapDto.setData(response);
+                wrapDto.setKey(productId);
+                tempList.add(wrapDto);
+            }
+            return wrapDto.getData();
         }else {
             return reviewService.retrieveReviewListByProductId(productId, pageable);
         }
     }
 
-    @Override
-    @Scheduled(fixedRate = 100)
-    public void updateCheck(){
-        List<Long> mapDeleteList = redisCacheService.updateCheck(REVIEW_PAGE_KEY_NAME);
-        for (Long productNo:mapDeleteList){
-            deleteFromMap(productNo);
-        }
-
-    }
-
-    private void deleteFromMap(Long productId){
-        ReviewResponseWrapDto wrapDto = reviewResponseWrapDtoHashMap.get(productId);
-        reviewResponseWrapDtoHashMap.remove(productId);
-        wrapDto.getPrevious().setNext(wrapDto.getNext());
-        wrapDto.getNext().setPrevious(wrapDto.getPrevious());
-        if (wrapDto==last){
-            last = null;
-        }
-        if (wrapDto==first){
-            first = null;
-        }
-    }
-
-    @Scheduled(cron = "0/1 * * * * *")
-    private void addProductAllInOneInMap(){
-        if (!tempList.isEmpty()){
-            synchronized (tempList){
-                while (!tempList.isEmpty()){
-                    ReviewResponseWrapDto productAllInOneResponse = tempList.get(0);
-                    tempList.remove(0);
-                    setWrapDtoToLast(productAllInOneResponse);
-                }
-            }
-            while(reviewResponseWrapDtoHashMap.size() > MAX_CACHE_NUM){
-                ReviewResponseWrapDto removeTarget = first;
-                reviewResponseWrapDtoHashMap.remove(removeTarget.getProductId());
-                first.getNext().setPrevious(null);
-                first = first.getNext();
-            }
-        }
-    }
-
-    private void setWrapDtoToLast(ReviewResponseWrapDto productAllInOneResponse) {
-        ReviewResponseWrapDto wrapDto;
-        if (reviewResponseWrapDtoHashMap.containsKey(productAllInOneResponse.getProductId())){
-            wrapDto = reviewResponseWrapDtoHashMap.get(productAllInOneResponse.getProductId());
-            unlinkExistDto(wrapDto);
-        }else {
-            wrapDto = productAllInOneResponse;
-            reviewResponseWrapDtoHashMap.put(wrapDto.getProductId(),wrapDto);
-        }
-        if (last == null){
-            last = wrapDto;
-        }
-        if (wrapDto != last){
-            wrapDto.setPrevious(last);
-            last.setNext(wrapDto);
-            last = wrapDto;
-        }
-        if(first == null){
-            first = wrapDto;
-        }
-    }
-
-    private static void unlinkExistDto(ReviewResponseWrapDto wrapDto) {
-        if (wrapDto.getNext() != null){
-            if(wrapDto.getPrevious() != null){
-                wrapDto.getPrevious().setNext(wrapDto.getNext());
-            }
-            wrapDto.getNext().setPrevious(wrapDto.getPrevious());
-        }
-    }
 
 }

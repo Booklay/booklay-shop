e08251a27e9367ee9b2a0dbcd32fb392fa28fe1f
opgumo <opgumo@naver.com>
2023-03-01 15:40:50 +0900

    test: 테스트 작성

--- src/test/java/com/nhnacademy/booklay/server/repository/product/ProductRepositoryTest.java
+++ src/test/java/com/nhnacademy/booklay/server/repository/product/ProductRepositoryTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
@@ -37,6 +38,41 @@ class ProductRepositoryTest {
 
         //then
         assertThat(expected.isDeleted()).isFalse();
+    }
 
+    @Test
+    @DisplayName("AuthorityRepository save test")
+    void findAllBooksForSubscribeBy() {
+        //given
+        PageRequest request = PageRequest.of(1, 1);
+
+        //when
+        productRepository.findAllBooksForSubscribeBy(request);
+
+        //then
+    }
+
+    @Test
+    @DisplayName("AuthorityRepository save test")
+    void findAuthorNameByProductId() {
+        //given
+        Long targetId = 1L;
+
+        //when
+        productRepository.findAuthorNameByProductId(targetId);
+
+        //then
+    }
+
+    @Test
+    @DisplayName("AuthorityRepository save test")
+    void findNotDeletedByPageable() {
+        //given
+        PageRequest request = PageRequest.of(1, 1);
+
+        //when
+        productRepository.findNotDeletedByPageable(request);
+
+        //then
     }
 }
